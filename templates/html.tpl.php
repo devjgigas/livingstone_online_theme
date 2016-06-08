8c65eec0a7587c0bcf50e510f02fddf7dc869678
Kathy Chavez <kappaluppa2@gmail.com>
2016-06-08 13:02:30 -0700

    Level 3 changes

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -6,6 +6,7 @@
 <title><?php print $head_title ?></title>
 <?php print $styles; ?>
 <?php print $scripts; ?>
+<meta name="format-detection" content="telephone=no"/>
 <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Crimson+Text:400,600,700,400italic,600italic|Patua+One' rel='stylesheet' type='text/css'>
 <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'lo') . '/js/html5.js'; ?>"></script><![endif]-->
